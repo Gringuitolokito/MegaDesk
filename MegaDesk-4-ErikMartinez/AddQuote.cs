716c8bb9c2defa5b78a8b350f03e04188dd63d4d
remote-upload <remoteupload@git.edu>
2019-01-28 22:58:35 -0700

    First commit of megadesk1.1

---
+++ MegaDesk-4-ErikMartinez/AddQuote.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_4_ErikMartinez
+{
+    public partial class AddQuote : Form
+    {
+        public AddQuote()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            var mainMenu = (MainMenu)Tag;
+            mainMenu.Show();
+            Close();
+        }
+
+        private void AddQuote_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var mainMenu = (MainMenu)Tag;
+            mainMenu.Show();
+        }
+
+        private void addNewQoute_Button_Click(object sender, EventArgs e)
+        {
+            var desk = new Desk();
+            desk.Width = Int32.Parse(width.Text);
+            desk.Depth = Int32.Parse(depth.Text);
+            desk.Drawers = Int32.Parse(drawers.Text);
+            
+        }
+    }
+}
---
+++ MegaDesk-4-ErikMartinez/Desk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_4_ErikMartinez
+{
+    public class Desk
+    {
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int Drawers { get; set; }
+        public string SurfaceMaterial { get; set; }
+
+        // Constraints for Width and Depth
+
+        public const int MINWIDTH = 24;
+        public const int MAXWIDTH = 96;
+        public const int MINDEPTH = 12;
+        public const int MAXDEPTH = 48;
+        public const int MINDRAWER = 0;
+        public const int MAXDRAWER = 7;
+    }
+}
